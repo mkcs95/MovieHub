aaf9dbb578f162f634e05c7f2ad68c71b9b87527
Abhishek Joshi <joshi.abhishek@hotmail.com>
2015-12-01 10:26:10 -0800

    main scaffolding + authentication

--- js/main.js
+++ js/main.js
@@ -12,7 +12,118 @@ myApp.config(function ($stateProvider) {
 	$stateProvider
 		.state('index', {
 			url: '',
-			templateUrl: 'templates/home.html',
+			templateUrl: 'templates/index.html',
 			controller: 'HomeController'
 		})
+		
+		.state('critic', {
+			url: '/critic',
+			templateUrl: 'templates/critic.html',
+			controller: 'HomeController'
+		})
+		
+		.state('favorites', {
+			url: '/favorites',
+			templateUrl: 'templates/favorites.html',
+			controller: 'HomeController'
+		})
+		
+		.state('explore', {
+			url: '/explore',
+			templateUrl: 'templates/explore.html',
+			controller: 'HomeController'
+		})
+		
+		.state('movie', {
+			url: '/movie',
+			templateUrl: 'templates/movie.html',
+			controller: 'HomeController'
+		})
+});
+
+myApp.controller('HomeController', function($scope, Auth, $firebaseArray, $firebaseObject) {
+	/***REFERENCES AND AUTH***/
+
+	var ref = new Firebase('https://.firebaseio.com/');
+
+	var usersRef = ref.child("users");
+
+	var favoritesRef = ref.child("favorites");
+
+    $scope.users = $firebaseObject(usersRef);
+
+	$scope.authObj = Auth;
+
+	$scope.authObj.$onAuth(function (authData) {
+		$scope.authData = authData
+
+		if (authData != null) {
+			$scope.user = $scope.users[authData.uid];
+		}
+		else {
+			$scope.user = null;
+		}
+
+	});
+
+	//Sign Up
+	$scope.signUp = function () {
+
+		$scope.authObj.$createUser({
+			email: $scope.email,
+			password: $scope.password,
+		})
+
+			.then($scope.logIn)
+
+		// Once logged in, set and save the user data
+			.then(function (authData) {
+				$scope.authData = authData;
+
+				//CHANGE
+				$scope.users[authData.uid] = {
+					firstName: $scope.firstName,
+					lastName: $scope.lastName,
+					favorites: null
+				}
+
+				$scope.users.$save()
+			})
+
+			.then(function () {
+				$('#signupModal').modal('hide');
+			})
+
+			.catch(function (error) {
+				console.error("Error: ", error);
+			});
+	}
+	
+	// SignIn function
+	$scope.signIn = function () {
+		$scope.logIn().then(function (authData) {
+			$scope.authData = authData;
+
+		})
+
+			.then(function () {
+				$('#loginModal').modal('hide');
+			})
+	}
+
+	// LogIn function
+	$scope.logIn = function () {
+		console.log('log in')
+		return $scope.authObj.$authWithPassword({
+			email: $scope.email,
+			password: $scope.password
+		})
+	}
+
+	// LogOut function
+	$scope.logOut = function () {
+		$scope.authObj.$unauth()
+		$scope.authData = null
+		$scope.user = null
+	}
 });
